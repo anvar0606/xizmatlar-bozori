ca28d6b6b5cf37a394c2691e01bd92499ee45089
anvar0606 <anvarqurbonniyozov9@gmail.com>
2026-04-18 00:36:38 +0500

    Create src/services/api.js file with axios configuration.

---
+++ src/services/api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+const API_BASE_URL = process.env.API_URL || 'http://localhost:5000/api';
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+api.interceptors.request.use(async (config) => {
+  const token = await AsyncStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => Promise.reject(error));
+export default api;
